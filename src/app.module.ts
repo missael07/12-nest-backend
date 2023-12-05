9340b34863289384046f5e9e7aec207e0f466aa5
Missael Padilla <lmpadillar@gmail.com>
2023-12-05 10:55:26 -0800

    install mongoose to conect with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot( process.env.MONGO_URI ),//'mongodb://localhost:27017'),
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
