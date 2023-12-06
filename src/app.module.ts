00b68f202f7d582736761c34bfa00d4c47912c2a
Missael Padilla <lmpadillar@gmail.com>
2023-12-05 21:52:05 -0800

    Setting up deployment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot( process.env.MONGO_URI ),
+    MongooseModule.forRoot( process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME} ),
     AuthModule
   ],
   controllers: [],
